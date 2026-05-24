818f929f495b339ec0b98d9144e93bc618ddabbd
Stepan Kuklis <panpanika@gmail.com>
2026-05-24 09:51:45 +0200

    switching to my repo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const octokit = new Octokit({
 await run({
   github: octokit,
   context: {
-    repo: { owner: "nornagon", repo: "cdda-data" },
+    repo: { owner: "panpanika", repo: "cdda-data" },
   },
   dryRun: !process.env.GITHUB_TOKEN,
 });
